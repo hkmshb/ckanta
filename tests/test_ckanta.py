d06a799a5177b9e3383b0fbafbb029e9be228a5a
Abdul-Hakeem Shaibu <hkmshb@gmail.com>
2018-09-12 09:33:08 +0100

    Add tests for ActionDef

--- tests/test_ckanta.py
+++ tests/test_ckanta.py
@@ -1,5 +1,12 @@
 from ckanta import __version__
+from ckanta import ActionDef
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+def test_actiondef():
+    action = ActionDef('action')
+    assert action is not None
+    assert action.table_def is None
