60c988a429e02e2f6f6d6b828f111844e60ddf19
Abdul-Hakeem Shaibu <hkmshb@gmail.com>
2018-09-18 12:15:04 +0100

    Provide 'membership list' command on the cli

--- ckanta/cli/__init__.py
+++ ckanta/cli/__init__.py
@@ -7,7 +7,8 @@ import logging
 from pprint import pprint
 from ckanta.common import read_config, get_config_instance, \
      ConfigError, ApiClient, Config
-from ckanta.commands import CommandError, ListCommand, ShowCommand
+from ckanta.commands import CommandError, ListCommand, ShowCommand, \
+     MembershipCommand
 
 
 _log = logging.getLogger(__name__)
@@ -35,8 +36,8 @@ def _load_config(instance_name):
 @click.option('-u', '--urlbase')
 @click.option('-k', '--apikey')
 @click.option('-i', '--instance', default='local')
-@click.option('--post', default=False, is_flag=True)
-@click.option('--debug', default=False, is_flag=True)
+@click.option('-p', '--post', default=False, is_flag=True)
+@click.option('-d', '--debug', default=False, is_flag=True)
 @click.pass_context
 def ckanta(ctx, urlbase, apikey, instance, post, debug):
     class CKANTAContext: 
@@ -66,15 +67,15 @@ def ckanta(ctx, urlbase, apikey, instance, post, debug):
 
 
 @ckanta.command()
-@click.option('-n', '--name', default='local')
+@click.option('-i', '--instance', default='local')
 @click.option('-l', '--list', default=False, is_flag=True)
 @click.option('--show-key', default=False, is_flag=True)
 @click.pass_obj
-def config(context, name, list, show_key):
+def config(context, instance, list, show_key):
     client = context.client
 
-    if list and name != 'local':
-        click.echo('error: The options -n/--name and -l/--list are '
+    if list and instance != 'local':
+        click.echo('error: The options -i/--instance and -l/--list are '
                    'mutually exclusive. Use just one!\n')
         sys.exit()
 
@@ -86,12 +87,12 @@ def config(context, name, list, show_key):
         click.echo()
 
     def _show_config():
-        if name == 'local':
+        if instance == 'local':
             _show_config_item(Config(client.urlbase, client.apikey))
             return
 
         try:
-            conf = _load_config(name)
+            conf = _load_config(instance)
             _show_config_item(conf)
         except ConfigError as ex:
             sys.exit()
@@ -149,14 +150,49 @@ def show(context, object, id, option):
         option
     ))
     _log.debug('parsed options: {}'.format(option_dict))
+    result = {}
 
     try:
         cmd = ShowCommand(context.client, object=object, id=id, **option_dict)
         result = cmd.execute(as_get=context.as_get)
-        pprint(result['result'])
     except CommandError as ex:
         func = _log.error if not context.debug else _log.exception
         func('error: {}'.format(ex))
+    pprint(result['result'])
+
+@ckanta.group()
+@click.pass_obj
+def membership(context):
+    '''Retrieve user membership across organizations and groups within a
+    CKAN instance.
+    '''
+    pass
+
+
+@membership.command('list')
+@click.argument('userid', type=str)
+@click.option('-g', '--check-groups', default=False, is_flag=True)
+@click.pass_obj
+def membership_list(context, userid, check_groups):
+    '''Retrieves and returns organizations which user with specified user
+    Id has membership.
+
+    :param userid: Id of user whose memberships are to be retrieved.
+    :type userid: string
+
+    :param check_groups: Indicates whether to retrieve groups for which the user
+        with specified user Id has membership.
+    :type check_groups: boolean
+    '''
+    result = {}
+    try:
+        cmd = MembershipCommand(context.client, userid, check_groups)
+        result = cmd.execute(as_get=context.as_get)
+    except CommandError as ex:
+        func = _log.error if not context.debug else _log.exception
+        func('error: {}'.format(ex))
+        return
+    pprint(result)
 
 
 if __name__ == '__main__':
